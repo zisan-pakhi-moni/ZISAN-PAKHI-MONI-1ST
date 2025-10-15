195b4c8ef26f98691bbea6d13d55e98bf816e7ce
MONI-ZISAN <massengerbotmz@gmail.com>
2025-10-15 18:47:16 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -35,7 +35,7 @@ module.exports.run = async function({ api, event }) {
 │ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
 ├───────────────
 │ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
-│ https://fb.com/100079776818351
+│ https://fb.com/61553612065795
 │ 💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽:
 │ https://wa.me/01931411945
 └───────────────⭓
@@ -49,7 +49,7 @@ module.exports.run = async function({ api, event }) {
  attachment: fs.createReadStream(__dirname + "/cache/owner.mp4")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.mp4"));
 
- return request("https://i.imgur.com/rPl0O7k.mp4")
+ return request("https://i.imgur.com/ai11602.mp4")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.mp4'))
  .on('close', () => callback());
 };
